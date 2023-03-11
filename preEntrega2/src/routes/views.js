b5a60e9df060c55f9ffeb907b28a782a806e088f
Estanislao Rey <tankysoluciones@gmail.com>
2023-03-11 13:27:53 -0300

    ordenado vistas

---
+++ preEntrega2/src/routes/views.js
@@ -0,0 +1,8 @@
+import ProductManager from "../dao/classes/MongoDB/ProductManager"
+import CartManager from "../dao/classes/MongoDB/CartManager"
+
+const router =Router()
+
+const productManager = new ProductManager
+const cartManager =new CartManager
+
