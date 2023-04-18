b610b38c6d4d20311486b38f00da550161bf3ad2
Estanislao Rey <tankysoluciones@gmail.com>
2023-04-18 19:56:32 -0300

    acomodando para la pre entre

--- 2practicaIntegradora/src/middleware/auth.js
+++ 2practicaIntegradora/src/middleware/auth.js
@@ -1,8 +1,8 @@
 function auth (req, res, next){
     console.log('auth: ',req.session)
-    // if (req.session?.user.name !== 'fede fede' || !req.session?.admin ) {
-    if (req.session?.user.name !== 'fede fede' ) {
-        return res.send('No estas autorizado para ver esta página, por favor')
+    // if (req.session?.user.name !== 'esani estani' || !req.session?.admin ) {
+    if (req.session?.user.name !== 'estani estani' ) {
+        return res.send('No estas autorizado para ver esta página')
     }
     
     return next()
