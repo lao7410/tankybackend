474c7cba27755edf2c97ba8aaf0fc4c5614f049c
Estanislao Rey <tankysoluciones@gmail.com>
2023-05-28 13:46:40 -0300

    actualizando proyecto final

--- ProyectoFinal/src/app.js
+++ ProyectoFinal/src/app.js
@@ -1,17 +1,54 @@
-const ProductManager = require('./productManager'); // importar la clase
+const ProductManager = require('./ProductManager');
 
-const productManager = new ProductManager(); // crear una instancia
+const productManager = new ProductManager();
 
 console.log(productManager.getProducts()); // []
-productManager.addProduct({
-  title: 'producto prueba',
-  description: 'Este es un producto prueba',
-  price: 200,
-  thumbnail: 'Sin imagen',
-  code: 'abc123',
-  stock: 25
-});
-console.log(productManager.getProducts()); // [{...}]
-productManager.addProduct('Producto de prueba', 'Este es un producto de prueba', 100, 'imagen.jpg', '123abc', 10);
-productManager.addProduct('Producto de prueba', 'Este es un producto de prueba', 100, 'imagen.jpg', '123abc', 10);
-console.log(productManager.getProductById(1))
+
+try {
+  productManager.addProduct({
+    title: 'producto prueba',
+    description: 'Este es un producto prueba',
+    price: 200,
+    thumbnail: 'Sin imagen',
+    code: 'abc123',
+    stock: 25
+  });
+  console.log(productManager.getProducts()); // [{...}]
+} catch (error) {
+  console.error('Error al agregar el producto:', error.message);
+}
+
+try {
+  productManager.addProduct({
+    title: 'Producto de prueba',
+    description: 'Este es un producto de prueba',
+    price: 100,
+    thumbnail: 'imagen.jpg',
+    code: '123abc',
+    stock: 10
+  });
+  console.log(productManager.getProducts()); // [{...}, {...}]
+} catch (error) {
+  console.error('Error al agregar el producto:', error.message);
+}
+
+try {
+  productManager.addProduct({
+    title: 'Producto de prueba',
+    description: 'Este es un producto de prueba',
+    price: 100,
+    thumbnail: 'imagen.jpg',
+    code: '123abc',
+    stock: 10
+  });
+  console.log(productManager.getProducts());
+} catch (error) {
+  console.error('Error al agregar el producto:', error.message);
+}
+
+try {
+  console.log(productManager.getProductById(1));
+  console.log(productManager.getProductById(2));
+} catch (error) {
+  console.error('Error al obtener el producto:', error.message);
+}
--- ProyectoFinal/src/productManager.js
+++ ProyectoFinal/src/productManager.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 class ProductManager {
   constructor(path) {
-    this.path = path || './data/products.json';
+    this.path = path || './src/data/products.json';
     try {
       const data = fs.readFileSync(this.path);
       this.products = JSON.parse(data);
@@ -19,6 +19,12 @@ class ProductManager {
     const lastProduct = this.products[this.products.length - 1];
     const newId = lastProduct ? lastProduct.id + 1 : 1;
     const newProduct = { id: newId, ...product };
+    
+    const isCodeDuplicate = this.products.some((p) => p.code === newProduct.code);
+    if (isCodeDuplicate) {
+      throw new Error('El código del producto ya existe');
+    }
+    
     this.products.push(newProduct);
     this.saveProductsToFile();
   }
@@ -28,18 +34,22 @@ class ProductManager {
   }
 
   getProductById(id) {
-    return this.products.find(product => product.id === id);
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+      throw new Error('Not found');
+    }
+    return product;
   }
 
   updateProduct(id, updatedProduct) {
-    const index = this.products.findIndex(product => product.id === id);
+    const index = this.products.findIndex((p) => p.id === id);
     if (index === -1) return;
     this.products[index] = { id, ...updatedProduct };
     this.saveProductsToFile();
   }
 
   deleteProduct(id) {
-    const index = this.products.findIndex(product => product.id === id);
+    const index = this.products.findIndex((p) => p.id === id);
     if (index === -1) return;
     this.products.splice(index, 1);
     this.saveProductsToFile();
@@ -47,55 +57,3 @@ class ProductManager {
 }
 
 module.exports = ProductManager;
-
-
-/* class ProductManager {
-    constructor() {
-      this.products = [];
-      this.lastId = 0;
-    }
-  
-    addProduct(title, description, price, thumbnail, code, stock) {
-      if (!title || !description || !price || !thumbnail || !code || !stock) {
-        console.error('All fields are required.');
-        return;
-      }
-      if (this.getProductByCode(code)) {
-        console.error(`Product with code "${code}" already exists.`);
-        return;
-      }
-      const product = {
-        id: ++this.lastId,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
-      };
-      this.products.push(product);
-      console.log(`Product with id "${product.id}" has been added.`);
-    }
-  
-    getProductByCode(code) {
-      return this.products.find(p => p.code === code);
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    getProductById(id) {
-      const product = this.products.find(p => p.id === id);
-      if (product) {
-        return product;
-      } else {
-        console.error(`Product with id "${id}" not found.`);
-      }
-    }
-  }
-  
-  module.exports = ProductManager; // exportar la clase
-
-
- */
